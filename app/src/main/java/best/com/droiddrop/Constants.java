12f5d603927b1ee9005795edae0e9aaa8dec6868
Walt <wpniederer@cpp.edu>
2019-02-24 22:11:31 -0800

    adding google sample to test connecting devices

---
+++ app/src/main/java/best/com/droiddrop/Constants.java
@@ -0,0 +1,6 @@
+package best.com.droiddrop;
+
+public class Constants {
+    /** A tag for logging. Use 'adb logcat -s DroidDrop' to follow the logs. */
+    public static final String TAG = "DroidDrop";
+}
