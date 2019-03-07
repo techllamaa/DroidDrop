037c79ba3a63db20e33df0b47046dc1a4ac12f14
Walt <wpniederer@cpp.edu>
2019-03-06 18:45:18 -0800

    Revert "creating test environment for making a connection"
    This reverts commit f0d75043e747dd717dd550763e5bdbe3a53eb9e1.

--- app/src/main/java/best/com/droiddrop/MainActivity.java
+++ app/src/main/java/best/com/droiddrop/MainActivity.java
@@ -1,113 +1,32 @@
 package best.com.droiddrop;
 
-import android.animation.Animator;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.annotation.ColorInt;
-import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
-import android.text.method.ScrollingMovementMethod;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.android.gms.nearby.connection.Strategy;
-
-import java.util.Random;
-
-public class MainActivity extends ConnectionsActivity {
-    public static final boolean DEBUG = true;
-    /** P2P_STAR is the higher bandwith connection type*/
-    private static final Strategy CONNECTION_STRATEGY = Strategy.P2P_STAR;
-
-    /**
-     * TODO: Remove hardcoded id once done testing connections
-     * This service id lets us find other nearby devices that are interested in the same thing.
-     * Hardcoded for testing purposes.
-     */
-    private static final String SERVICE_ID = "ConnectionTest";
-
-    /** A random ID used as device's name. */
-    private String mName;
-
-    /** Background colors for determining whether connection was made between two devices */
-    @ColorInt
-    private static final int[] COLORS =
-            new int[] {
-                    0xFFF44336 /* red */,
-                    0xFF9C27B0 /* deep purple */,
-                    0xFF00BCD4 /* teal */,
-                    0xFF4CAF50 /* green */,
-                    0xFFFFAB00 /* amber */,
-                    0xFFFF9800 /* orange */,
-                    0xFF795548 /* brown */
-            };
-
-    /** State of the app */
-    private State appState = State.UNKNOWN;
-
-    @ColorInt
-    private int appConnected = COLORS[0];
-
-
-    /** An animator that controls the animation from previous state to current state. */
-    @Nullable
-    private Animator mCurrentAnimator;
-
-    /** Various state for animation transitions */
-    private TextView mPreviousStateView;
-    private TextView mCurrentStateView;
-
-    private TextView mDebugLogView;
+public class MainActivity extends AppCompatActivity{
+    private boolean connection = false;
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        getSupportActionBar()
-                .setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.actionBar));
-
-        mPreviousStateView = (TextView) findViewById(R.id.previous_state);
-        mCurrentStateView = (TextView) findViewById(R.id.current_state);
-
-        mDebugLogView = (TextView) findViewById(R.id.debug_log);
-        mDebugLogView.setVisibility(DEBUG ? View.VISIBLE : View.GONE);
-        mDebugLogView.setMovementMethod(new ScrollingMovementMethod());
-
-        mName = generateRandomName();
-
-        ((TextView) findViewById(R.id.name)).setText(mName);
     }
 
-
-    @Override
-    protected String getName() {
-        return null;
-    }
-
-    @Override
-    protected String getServiceId() {
-        return null;
-    }
-
-    @Override
-    protected Strategy getStrategy() {
-        return null;
-    }
-
-    private static String generateRandomName() {
-        String name = "";
-        Random random = new Random();
-        for (int i = 0; i < 5; i++) {
-            name += random.nextInt(10);
+    /** called when user presses send button */
+    public void connectButton(View view) {
+        TextView textView = findViewById(R.id.textView2);
+        //call connection here, give feedback flag connection
+        if(connection){
+            textView.setText("Connected");
         }
-        return name;
+        else{
+            textView.setText("Not Connected");
+        }
     }
-
-    /** UI states */
-    public enum State {
-        UNKNOWN,
-        SEARCHING,
-        CONNECTED
-    }
-
 }
