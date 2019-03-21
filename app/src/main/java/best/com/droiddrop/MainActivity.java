881da8501e14ef7b478d9d5c967c40c9cf56d517
unknown <lesliecovarrubias95@gmail.com>
2019-03-20 21:31:13 -0700

    Floating action button created with certain functionalities. Tab added with no functionality

--- app/src/main/java/best/com/droiddrop/MainActivity.java
+++ app/src/main/java/best/com/droiddrop/MainActivity.java
@@ -1,5 +1,6 @@
 package best.com.droiddrop;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -8,25 +9,36 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
-    private boolean connection = false;
-    public MainActivity() {
-    }
+    private boolean open = true;
+
+    //ideally wanna get this number from backend
+    private int numOfDevicesFound = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton fab = findViewById(R.id.fabNearBy);
+        final FloatingActionButton foundDeviceOne = findViewById(R.id.fab_user);
+        final TextView device1 = findViewById(R.id.device1);
+        fab.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                if(open){
+                    foundDeviceOne.show();
+                    device1.setVisibility(View.VISIBLE);
+                    open = false;
+                }else{
+                    foundDeviceOne.hide();
+                    open = true;
+                }
+            }
+        });
     }
 
-    /** called when user presses send button */
-    public void connectButton(View view) {
-        TextView textView = findViewById(R.id.textView2);
-        //call connection here, give feedback flag connection
-        if(connection){
-            textView.setText("Connected");
-        }
-        else{
-            textView.setText("Not Connected");
-        }
+
+    protected void createContacts(){
+
+
+
     }
 }
