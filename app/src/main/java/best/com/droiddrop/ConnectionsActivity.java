d553c3c1c553384400d776092f8ad1736bc43bc9
Walt <wpniederer@cpp.edu>
2019-05-05 23:13:19 -0700

    fine location

--- app/src/main/java/best/com/droiddrop/ConnectionsActivity.java
+++ app/src/main/java/best/com/droiddrop/ConnectionsActivity.java
@@ -55,6 +55,7 @@ public abstract class ConnectionsActivity extends AppCompatActivity {
                     Manifest.permission.ACCESS_WIFI_STATE,
                     Manifest.permission.CHANGE_WIFI_STATE,
                     Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.READ_EXTERNAL_STORAGE,
                     Manifest.permission.WRITE_EXTERNAL_STORAGE
             };
